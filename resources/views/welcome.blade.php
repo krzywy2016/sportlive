494edbc3679c052b5d3042dacd455f2bd0f252d1
Kamil Krzywonos <kamilkrzywonos@gmail.com>
2019-04-13 12:50:22 +0200

    adding styles to a website and creating a sports relationship view

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -1,95 +1,119 @@
-<!doctype html>
-<html lang="{{ app()->getLocale() }}">
-    <head>
-        <meta charset="utf-8">
-        <meta http-equiv="X-UA-Compatible" content="IE=edge">
-        <meta name="viewport" content="width=device-width, initial-scale=1">
-
-        <title>Laravel</title>
-
-        <!-- Fonts -->
-        <link href="https://fonts.googleapis.com/css?family=Raleway:100,600" rel="stylesheet" type="text/css">
-
-        <!-- Styles -->
-        <style>
-            html, body {
-                background-color: #fff;
-                color: #636b6f;
-                font-family: 'Raleway', sans-serif;
-                font-weight: 100;
-                height: 100vh;
-                margin: 0;
-            }
-
-            .full-height {
-                height: 100vh;
-            }
-
-            .flex-center {
-                align-items: center;
-                display: flex;
-                justify-content: center;
-            }
-
-            .position-ref {
-                position: relative;
-            }
-
-            .top-right {
-                position: absolute;
-                right: 10px;
-                top: 18px;
-            }
-
-            .content {
-                text-align: center;
-            }
-
-            .title {
-                font-size: 84px;
-            }
-
-            .links > a {
-                color: #636b6f;
-                padding: 0 25px;
-                font-size: 12px;
-                font-weight: 600;
-                letter-spacing: .1rem;
-                text-decoration: none;
-                text-transform: uppercase;
-            }
-
-            .m-b-md {
-                margin-bottom: 30px;
-            }
-        </style>
-    </head>
-    <body>
-        <div class="flex-center position-ref full-height">
-            @if (Route::has('login'))
-                <div class="top-right links">
-                    @auth
-                        <a href="{{ url('/home') }}">Home</a>
-                    @else
-                        <a href="{{ route('login') }}">Login</a>
-                        <a href="{{ route('register') }}">Register</a>
-                    @endauth
-                </div>
-            @endif
-
-            <div class="content">
-                <div class="title m-b-md">
-                    Laravel
-                </div>
-
-                <div class="links">
-                    <a href="https://laravel.com/docs">Documentation</a>
-                    <a href="https://laracasts.com">Laracasts</a>
-                    <a href="https://laravel-news.com">News</a>
-                    <a href="https://forge.laravel.com">Forge</a>
-                    <a href="https://github.com/laravel/laravel">GitHub</a>
-                </div>
-            </div>
-        </div>
-    </body>
+<html xmlns="http://www.w3.org/1999/xhtml" xml:lang="pl-PL" lang="pl-PL" >
+	<head>
+            <title>Sport LIVE</title>
+			<meta name="viewport" content="width=device-width, initial-scale=1">
+			<meta name="Description" content="" />
+			<meta name="Keywords" content="" />
+			<meta http-equiv="content-type" content="text/html; charset=utf-8" />
+			<meta name="Generator" content="Aisen CMS 1.0" />
+			<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.0/css/bootstrap.min.css">
+			<link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.8.1/css/all.css" integrity="sha384-50oBUHEmvpQ+1lW4y57PTFmhCaXp0ML5d60M1M7uH2+nqUivzIebhndOJK28anvf" crossorigin="anonymous">
+			<link href="https://fonts.googleapis.com/css?family=Montserrat" rel="stylesheet"> 
+			<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
+			<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.0/js/bootstrap.min.js"></script>
+			<link href="{{ asset('css/style.css') }}" rel="stylesheet" />
+			<script async src="//pagead2.googlesyndication.com/pagead/js/adsbygoogle.js"></script>
+	</head>
+	<body>
+			<!-- top menu -->
+			<div class="container-fluid topmenu">
+				<div class="container nopadding">
+					<div class="col-lg-12 topmenutext">
+						<div class="topmenubox"><a href="#" >Strona główna</a></div>
+						<div class="topmenubox"><a href="#" >Dodaj relację</a></div>
+						<div class="topmenubox"><a href="#" >Regulamin</a></div>
+						<div class="topmenubox"><a href="#" >O nas</a></div>
+					</div>
+				</div>
+			</div>
+			<!-- top background image -->
+			<div class="container-fluid">
+				<div class="row">
+					<div class="col-lg-12 topbackground">
+						tlo top menu
+					</div>
+				</div>
+			</div>
+			<!-- content -->
+			<div class="container">
+				<div class="row">
+					<div class="col-lg-8 relation">
+						<div class="row">
+							<div class="col-lg-5">
+						
+							</div>
+							<div class="col-lg-2">
+								czas
+							</div>
+							<div class="col-lg-5">
+						
+							</div>
+						</div>
+						<div class="row">
+							<div class="col-lg-5">
+								zespół gospodarzy
+							</div>
+							<div class="col-lg-2">
+								wynik
+							</div>
+							<div class="col-lg-5">
+								zespół gości
+							</div>
+						</div>
+						<div class="row">
+							<div class="col-lg-12">
+								dane wydarzenia jak data, godzina, miejsce, liga
+							</div>
+						</div>
+						<div class="row">
+							<div class="col-lg-12">
+								reklama
+							</div>
+						</div>
+						<div class="row">
+							<div class="col-lg-12">
+								rekordy z wydarzeniami i zdjęciami z boiska
+							</div>
+						</div>
+					</div>
+					<div class="col-lg-4 promotion">
+						<div class="row">
+							<div class="col-lg-12">
+								fejsbuczek
+							</div>
+							<div class="col-lg-12">
+								Skład drużyny gospodarzy
+							</div>
+							<div class="col-lg-12">
+								Skład drużyny gości
+							</div>
+							<div class="col-lg-12">
+								Zmiany
+							</div>
+							<div class="col-lg-12">
+								kartki
+							</div>
+							<div class="col-lg-12">
+								Czat
+							</div>
+						</div>
+					</div>
+				</div>
+			</div>
+			<!-- footer -->
+			<div class="container-fluid">
+				<div class="row">
+					<div class="col-lg-12 footer">
+						tlo top menu
+					</div>
+				</div>
+			</div>
+			<div class="container-fluid">
+				<div class="row">
+					<div class="col-lg-12 datefooter">
+						AISEN SportLive 1.0
+					</div>
+				</div>
+			</div>
+	</body>
 </html>
